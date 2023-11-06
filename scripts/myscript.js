76ba77ad20fe3800cfeaf163681ca85109a7722e
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-11-05 20:41:27 -0500

    myscript file updated

---
+++ scripts/myscript.js
@@ -0,0 +1,102 @@
+import { fetchRawMealData } from './index.js';
+document.addEventListener('DOMContentLoaded', () => {
+    const result = document.getElementById('wrapper');
+    const searchBtn = document.getElementById('searchButton');
+    const error = document.getElementById('error');
+    const userInput = document.getElementById('recipeInput');
+    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+    error.style.display = 'none';
+    searchBtn.addEventListener('click', async () => {
+        const inputValue = userInput.value;
+        if (inputValue.length == 0) {
+            error.style.display = 'block';
+        } else {
+            let searchQuery = inputValue;
+            fetchMealData(searchQuery);
+        }
+    });
+    
+    async function fetchMealData(searchQuery) {
+        try {
+            const response = await fetch(url + searchQuery);
+            const data = await response.json();
+            
+            result.innerHTML = '';
+            
+            if (data.meals) {
+                data.meals.forEach((myMeal) => {
+                    
+                    const showRecipeButton = document.createElement('button');
+                    showRecipeButton.className = 'myBtn showRecipee';
+                    showRecipeButton.textContent = 'Show Recipe';
+                    
+                    const detailCard = document.createElement('div');
+                    detailCard.className = 'detail-card';
+                    detailCard.innerHTML = `
+                    <img class="detail-img" src="${myMeal.strMealThumb}" alt="${myMeal.strMeal}">
+                    <div class="detail-desc">
+                    <div class="detail-name">
+                    <h4>${myMeal.strMeal}</h4>
+                    <p class="detail-sub">${myMeal.strArea}</p>
+                    </div>
+                    </div>
+                    `;
+                    
+                    detailCard.querySelector('.detail-desc').appendChild(showRecipeButton);
+                    
+                    showRecipeButton.addEventListener('click', () => {
+                        window.location.href = `recipe.html?q=${myMeal.strMeal}`;
+                    });
+                    
+                    result.appendChild(detailCard);
+                });
+            } else {
+                result.innerHTML = '<p>No meals found for the given query.</p>';
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+    
+    fetchRawMealData()
+});
+async function fetchSelectedMealData(searchQuery) {
+    const result = document.getElementById('wrapper');
+    const recipeeInstructions = document.getElementById('recipeeInstructions');
+    const recipeeIngredients = document.querySelector('.recipee-ingredients');
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        let myMeal = data.meals;
+
+        for (let i = 0; i < myMeal.length; i++) {
+            let meal = myMeal[i];
+            let count = 1;
+            let ingredients = [];
+            for (let j = 1; j <= 24; j++) {
+                if (meal['strIngredient' + j]) {
+                    ingredients.push(`${meal['strMeasure' + j]} ${meal['strIngredient' + j]}`);
+                }
+            }
+            console.log(ingredients);
+            const steps = meal.strInstructions.split('\r\n');
+            const formattedInstructions = steps.join('<br>');
+            recipeeInstructions.innerHTML = formattedInstructions;
+
+            let ingredientsList = '<ul>';
+            ingredients.forEach((ingredient) => {
+                ingredientsList += `<li>${ingredient}</li>`;
+            });
+            ingredientsList += '</ul>';
+            recipeeIngredients.innerHTML = ingredientsList;
+        }
+        
+        
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+export { fetchSelectedMealData };
--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,1 +1,10 @@
-import * as Mainfunc from "./scripts/index.js"
+// import * as Mainfunc from "./scripts/index.js"
+import { fetchSelectedMealData } from './myscript.js';
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const urlQuery = urlSearchParams.get("q");
+
+const recipeeName = document.querySelector('.recipee-name')
+recipeeName.textContent = urlQuery
+
+fetchSelectedMealData(urlQuery);
